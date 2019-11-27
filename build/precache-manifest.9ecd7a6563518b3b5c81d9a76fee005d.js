098b64deaec0163fc5e445dadbdeeea57bb952a5
Michał Barwicki <m.barwicki93@gmail.com>
2019-11-27 21:59:11 +0100

    Add build folder

---
+++ build/precache-manifest.9ecd7a6563518b3b5c81d9a76fee005d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641321ad1bc37ac806ccd31c39209aed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efff16b79893ffa67390",
+    "url": "/static/css/main.22f3b5a9.chunk.css"
+  },
+  {
+    "revision": "9102615cba6d1ecef94f",
+    "url": "/static/js/2.8ea5f895.chunk.js"
+  },
+  {
+    "revision": "efff16b79893ffa67390",
+    "url": "/static/js/main.91dc1c9f.chunk.js"
+  },
+  {
+    "revision": "21a2cf2096d496cae811",
+    "url": "/static/js/runtime-main.f8cfdc02.js"
+  }
+]);
